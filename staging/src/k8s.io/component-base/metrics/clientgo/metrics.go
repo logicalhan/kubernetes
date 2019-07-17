5625684d1f92ef1a807401317486fc10faf07198
Han Kang <hankang@google.com>
2019-07-16 17:45:03 -0700

    add leaderelection shared metrics and add a wrapper around clientgo metrics for bundled registration

---
+++ staging/src/k8s.io/component-base/metrics/clientgo/metrics.go
@@ -0,0 +1,30 @@
+/*
+Copyright 2019 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package clientgo
+
+import (
+	k8smetrics "k8s.io/component-base/metrics"
+	"k8s.io/component-base/metrics/clientgo/leaderelection"
+	"k8s.io/component-base/metrics/clientgo/restclient"
+	"k8s.io/component-base/metrics/clientgo/workqueue"
+)
+
+func RegisterMetrics(registry k8smetrics.KubeRegistry) {
+	leaderelection.RegisterMetric(registry)
+	workqueue.RegisterMetrics(registry)
+	restclient.RegisterMetrics(registry)
+}
